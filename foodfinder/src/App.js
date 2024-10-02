23032b0bfa3b69586e8c41fcfbc7cad42f8d5d40
Oscar Campos <oscarc691@gmail.com>
2024-10-02 14:01:28 -0600

    added some css and more js

--- foodfinder/src/App.js
+++ foodfinder/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Recipe from "./pages/Recipe";
+import Categories from "./pages/Categories";
+import Nav from "./components/Nav";
+import { useEffect, useState } from "react";
+import RecipeService from "./service/service";
+import SearchRecipes from "./pages/SearchRecipes";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+
+  // get the recipe categories
+  const getRecipeCategories = async () => {
+    try {
+      const { categories } = await RecipeService.fetchRecipeCategories();
+      setCategories(categories);
+    } catch (error) {
+      console.error("Error getting recipe categories", error);
+    }
+  };
+  useEffect(() => {
+    getRecipeCategories();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/recipes/:name"
+            element={<Recipes categories={categories} />}
+          />
+          <Route path="/recipes/:name/:id" element={<Recipe />} />
+          <Route
+            path="/categories"
+            element={<Categories categories={categories} />}
+          />
+          <Route
+            path="/search"
+            element={<SearchRecipes categories={categories} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
