f75ee0c08af5508a25f652040b53f757800b9b43
Oscar Campos <oscarc691@gmail.com>
2024-11-08 13:27:01 -0700

    Again more styling

--- foodfinder/src/components/Modal.jsx
+++ foodfinder/src/components/Modal.jsx
@@ -11,11 +11,11 @@ const Modal = ({ recipeVideo, onClose }) => {
         </button>
         {videoId && (
           <iframe
-            width="560"
-            height="315"
+            width="580"
+            height="335"
             src={`https://www.youtube.com/embed/${videoId}`}
             title="YouTube video player"
-            frameBorder="0"
+        
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
